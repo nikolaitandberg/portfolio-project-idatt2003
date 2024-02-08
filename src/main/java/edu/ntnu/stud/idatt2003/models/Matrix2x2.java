9b66598b40c7b3a28f1bbe9c5985f3821db8c8ea
Nikolai Tandberg <nikoltan@stud.ntnu.no>
2024-02-08 13:58:21 +0100

    chore: Clean up checkstyle violations

--- src/main/java/edu/ntnu/stud/idatt2003/models/Matrix2x2.java
+++ src/main/java/edu/ntnu/stud/idatt2003/models/Matrix2x2.java
@@ -9,11 +9,19 @@ package edu.ntnu.stud.idatt2003.models;
  * @since 2024-02-08
  */
 public class Matrix2x2 {
-  private double a00;
-  private double a01;
-  private double a10;
-  private double a11;
+  private final double a00;
+  private final double a01;
+  private final double a10;
+  private final double a11;
 
+  /**
+   * Constructor for the matrix.
+   *
+   * @param a00 First element in the first row
+   * @param a01 Second element in the first row
+   * @param a10 First element in the second row
+   * @param a11 Second element in the second row
+   */
   public Matrix2x2(double a00, double a01, double a10, double a11) {
     this.a00 = a00;
     this.a01 = a01;
@@ -21,9 +29,16 @@ public class Matrix2x2 {
     this.a11 = a11;
   }
 
+  /**
+   * Method for multiplying a matrix with a vector.
+   *
+   * @param vector Vector to multiply with
+   * @return Result of the multiplication
+   */
   public Vector2D multiply(Vector2D vector) {
     return new Vector2D(
-            this.a00 * vector.getX0() + this.a01 * vector.getX1(), this.a10 * vector.getX0() + this.a11 * vector.getX1()
+            this.a00 * vector.getX0() + this.a01 * vector.getX1(),
+            this.a10 * vector.getX0() + this.a11 * vector.getX1()
     );
   }
 
--- src/main/java/edu/ntnu/stud/idatt2003/models/Vector2D.java
+++ src/main/java/edu/ntnu/stud/idatt2003/models/Vector2D.java
@@ -8,9 +8,15 @@ package edu.ntnu.stud.idatt2003.models;
  * @since 2024-02-01
  */
 public class Vector2D {
-  private double x0;
-  private double x1;
+  private final double x0;
+  private final double x1;
 
+  /**
+   * Constructor for the vector.
+   *
+   * @param x0 First element in the vector
+   * @param x1 Second element in the vector
+   */
   public Vector2D(double x0, double x1) {
     this.x0 = x0;
     this.x1 = x1;
@@ -24,10 +30,22 @@ public class Vector2D {
     return x1;
   }
 
+  /**
+   * Method for adding two vectors together.
+   *
+   * @param vector Vector to add
+   * @return Result of the addition
+   */
   public Vector2D add(Vector2D vector) {
     return new Vector2D(this.x0 + vector.x0, this.x1 + vector.x1);
   }
 
+  /**
+   * Method for subtracting two vectors.
+   *
+   * @param vector Vector to subtract
+   * @return Result of the subtraction
+   */
   public Vector2D subtract(Vector2D vector) {
     return new Vector2D(this.x0 - vector.x0, this.x1 - vector.x1);
   }
