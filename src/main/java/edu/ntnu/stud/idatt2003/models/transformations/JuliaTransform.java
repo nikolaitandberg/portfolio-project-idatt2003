2556818d3fff489ab1622f5349adc3dc42a6ca39
Nikolai Tandberg <nikoltan@stud.ntnu.no>
2024-02-22 17:23:20 +0100

    fix(JuliaTransform): fix math in julia transformation

--- src/main/java/edu/ntnu/stud/idatt2003/models/transformations/JuliaTransform.java
+++ src/main/java/edu/ntnu/stud/idatt2003/models/transformations/JuliaTransform.java
@@ -9,7 +9,7 @@ import edu.ntnu.stud.idatt2003.models.Vector2D;
  * @version 1.0
  * @since 2024-02-22
  */
-public class JuliaTranform implements Transform2D {
+public class JuliaTransform implements Transform2D {
 
   private final Complex point;
   private final int sign;
@@ -21,7 +21,7 @@ public class JuliaTranform implements Transform2D {
    * @param sign  Sign of the transformation
    * @throws IllegalArgumentException If the sign is not 1 or -1
    */
-  public JuliaTranform(Complex point, int sign) {
+  public JuliaTransform(Complex point, int sign) {
     this.point = point;
     if (sign != 1 && sign != -1) {
       throw new IllegalArgumentException("Sign must be 1 or -1");
@@ -37,9 +37,10 @@ public class JuliaTranform implements Transform2D {
    */
   @Override
   public Vector2D transform(Vector2D point) {
-    point.subtract(this.point);
 
-    Complex complexPoint = (Complex) point;
+    Vector2D subtractedPoint = point.subtract(this.point);
+
+    Complex complexPoint = new Complex(subtractedPoint.getX0(), subtractedPoint.getX1());
 
     if (sign == -1) {
       Complex negativeComplex = new Complex(-complexPoint.getX0(), -complexPoint.getX1());
