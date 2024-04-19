357bbbb1e5f16347073b5cbd0ff6eaea747901a8
Nikolai Tandberg <nikoltan@stud.ntnu.no>
2024-04-19 21:15:56 +0200

    feat(view): add option of julia and affine and extra transforms

--- src/main/java/edu/ntnu/stud/idatt2003/view/MainView.java
+++ src/main/java/edu/ntnu/stud/idatt2003/view/MainView.java
@@ -12,6 +12,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MainView extends Application {
@@ -38,64 +39,57 @@ public class MainView extends Application {
     menuBar.getMenus().add(fractalMenu);
 
     VBox leftPanel = new VBox();
-    leftPanel.setSpacing(15); // Mellomrom mellom komponentene
+    leftPanel.setSpacing(15);
 
     ComboBox<String> savedFractalsDropdown = new ComboBox<>();
     leftPanel.getChildren().add(savedFractalsDropdown);
 
+    Text transformText = new Text("Choose transform type");
+    leftPanel.getChildren().add(transformText);
+
+
     ComboBox<String> fractalTypeDropdown = new ComboBox<>();
-    fractalTypeDropdown.getItems().addAll("Julia Set", "Sierpinski triangle", "Barnsley fern");
+    fractalTypeDropdown.getItems().addAll("Julia", "Affine");
     leftPanel.getChildren().add(fractalTypeDropdown);
     VBox fractalBox = new VBox();
-    fractalTypeDropdown.setOnAction(event -> {
-      String selected = fractalTypeDropdown.getValue();
-      if (selected.equals("Julia Set")) {
+
+
+    fractalTypeDropdown.setOnAction(event -> {String selected = fractalTypeDropdown.getValue();
+        if (selected.equals("Julia")) {
           fractalBox.getChildren().clear();
-        HBox juliaBox = new HBox();
-        TextField c = new TextField();
-          c.setPromptText("c");
-          Button submitC = new Button("Beregn");
-          juliaBox.getChildren().addAll(c, submitC);
-          fractalBox.getChildren().add(juliaBox);
-          leftPanel.getChildren().add(fractalBox);
+          fractalBox.getChildren().add(createJuliaBox());
         } else {
           fractalBox.getChildren().clear();
-          HBox vectorBox = new HBox();
-          HBox matrixBox1 = new HBox();
-          HBox matrixBox2 = new HBox();
-          TextField vector1 = new TextField();
-          vector1.setPromptText("Vector 1");
-          TextField vector2 = new TextField();
-          vector2.setPromptText("Vector 2");
-          Button submitVectors = new Button("Beregn");
-          vectorBox.getChildren().addAll(vector1, vector2, submitVectors);
-          TextField matrix1 = new TextField();
-          matrix1.setPromptText("Matrix 1");
-          TextField matrix2 = new TextField();
-          matrix2.setPromptText("Matrix 2");
-          matrixBox1.getChildren().addAll(matrix1, matrix2);
-          TextField matrix3 = new TextField();
-          matrix3.setPromptText("Matrix 3");
-          TextField matrix4 = new TextField();
-          matrix4.setPromptText("Matrix 4");
-          Button submitMatrix = new Button("Beregn");
-          matrixBox2.getChildren().addAll(matrix3, matrix4, submitMatrix);
-          fractalBox.getChildren().addAll(vectorBox, matrixBox1, matrixBox2);
-          leftPanel.getChildren().add(fractalBox);
+          fractalBox.getChildren().add(createAffineBox());
         }
-        Button save = new Button("Save");
-        leftPanel.getChildren().add(save);
+      leftPanel.getChildren().remove(fractalBox); // Remove the old fractalBox from leftPanel
+      leftPanel.getChildren().add(fractalBox); // Add the new fractalBox to leftPanel
       }
     );
 
-    HBox stepsBox = new HBox();
 
+    //button for saving current transformations
+    Button save = new Button("Save");
+    leftPanel.getChildren().add(save);
+
+    // field and button for running steps
+    HBox stepsBox = new HBox();
     TextField stepsField = new TextField();
     stepsField.setPromptText("Antall steg");
     Button submitSteps = new Button("Beregn");
-
     stepsBox.getChildren().addAll(stepsField, submitSteps);
 
+    // button for adding transformations
+    Button addTransformation = new Button("Add transformation");
+    addTransformation.setOnAction(actionEvent -> {String selected = fractalTypeDropdown.getValue();
+      if (selected.equals("Julia")) {
+        fractalBox.getChildren().add(createJuliaBox());
+      } else {
+        fractalBox.getChildren().add(createAffineBox());
+      }
+    });
+
+    leftPanel.getChildren().add(addTransformation);
     leftPanel.getChildren().add(stepsBox);
 
     BorderPane root = new BorderPane();
@@ -108,5 +102,45 @@ public class MainView extends Application {
     primaryStage.setTitle("Chaos Game");
     primaryStage.setScene(scene);
     primaryStage.show();
+  }
+
+  private HBox createJuliaBox() {
+    HBox juliaBox = new HBox();
+    TextField realPart = new TextField();
+    realPart.setPromptText("Real part");
+    TextField imaginaryPart = new TextField();
+    imaginaryPart.setPromptText("Imaginary part");
+    juliaBox.getChildren().addAll(realPart, imaginaryPart);
+    return juliaBox;
+  }
+
+  private HBox createAffineBox() {
+    HBox vectorAndMatrixBoxes = new HBox();
+    VBox vectorBox = new VBox();
+    TextField vector1 = new TextField();
+    vector1.setPromptText("Vector 1");
+    TextField vector2 = new TextField();
+    vector2.setPromptText("Vector 2");
+    vectorBox.getChildren().addAll(vector1, vector2);
+
+    VBox matricesBox = new VBox();
+
+    HBox matrixBox1 = new HBox();
+    TextField matrix1 = new TextField();
+    matrix1.setPromptText("Matrix 00");
+    TextField matrix2 = new TextField();
+    matrix2.setPromptText("Matrix 01");
+    matrixBox1.getChildren().addAll(matrix1, matrix2);
+
+    HBox matrixBox2 = new HBox();
+    TextField matrix3 = new TextField();
+    matrix3.setPromptText("Matrix 10");
+    TextField matrix4 = new TextField();
+    matrix4.setPromptText("Matrix 11");
+    matrixBox2.getChildren().addAll(matrix3, matrix4);
+
+    matricesBox.getChildren().addAll(matrixBox1, matrixBox2);
+    vectorAndMatrixBoxes.getChildren().addAll(vectorBox, matricesBox);
+    return vectorAndMatrixBoxes;
   }
 }
